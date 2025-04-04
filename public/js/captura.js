ca82ff8150cfe2df02df443343f2aacf232352f6
IngSaul004 <ing.sau.rivera004@.gmail.com>
2025-04-04 17:37:46 -0600

    se actaulizo el css y el html tambien se agrego capturar y unas funciones sencillas

---
+++ public/js/captura.js
@@ -0,0 +1,24 @@
+//funciona para habilitar los popups
+document.getElementById('guardar').addEventListener('click', function (event){
+    event.preventDefault();
+    const listado = document.getElementById('listado');
+    listado.style.display = "flex";
+    const formulario = document.getElementById('formulario');
+    formulario.style.display = "none";
+});
+
+document.getElementById('verLista').addEventListener('click', function (event){
+    event.preventDefault();
+    const listado = document.getElementById('imprimirListado');
+    listado.style.display = "flex";
+});
+
+document.getElementById('footer').addEventListener('click', function(){
+    const popup = document.getElementById('popup');
+    popup.style.display = "flex";
+ });
+
+ document.getElementById('cerrar-btn').addEventListener('click', function(){
+    const popup = document.getElementById('popup');
+    popup.style.display = "none";
+ });
